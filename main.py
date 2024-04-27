9b5c45a3e91dbe722253473e0083c70dfe607b5a
caglenoah <caglenoah@gmail.com>
2024-04-26 20:00:01 -0600

    added organise string into dictionary

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ def char_count(text):
     for char in lowered_text:
         if char.isalpha():
             if char in char_counts:
-                char_counts[char] +=1
-            else: char_counts[char] = 1
-    print(char_counts)
-
-
-    
+                char_counts[char] += 1
+            else:
+                char_counts[char] = 1
+    return char_counts
 
 def main():
     path_to_file = "books/frankenstein.txt"
@@ -21,7 +19,16 @@ def main():
         file_contents = f.read()
     total_words = count_words(file_contents)
     print("Total words in the book:", total_words)
-    char_count(file_contents)
+    char_counts = char_count(file_contents)
+    print("--- Begin report of", path_to_file, "---")
+    print(total_words, "words found in document")
+
+    char_list = [{"char": char, "count": count} for char, count in char_counts.items()]
+    char_list.sort(reverse=True, key=lambda x: x["count"])
+
+    for char_data in char_list:
+        print(f"The '{char_data['char']}' character was found {char_data['count']} times")
+    print("--- End report ---")
 
 if __name__ == "__main__":
     main()
